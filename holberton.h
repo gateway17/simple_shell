81af7baa15b5eb91ff04f5dbb9609fbae521f084
gateway17 <70966797+gateway17@users.noreply.github.com>
2020-11-27 11:27:48 -0500

    Update holberton.h

--- holberton.h
+++ holberton.h
@@ -18,28 +18,28 @@ void exc_process(char **token_line, char **token_path);
 void non_interactive_mode(void);
 void interactive_mode(void);
 
-/* TOOLS_1 */
+/* funcs_1 */
 char  **our_strtok(char *str, const char *delim);
 int count_words(char *str, char *delim);
 
-/* TOOLS_2 */
+/* funcs_2 */
 int largo(char *string);
 void ultimonulo(char *linea);
 char *buscar_path(void);
 int num_tokens(char **str);
 int _strcmpn(char *s1, char *s2, int n);
 
-/* TOOLS_3 */
+/* funcs_3 */
 char *our_strcat(char *dest, char *src);
 void special_free(char **cartman);
 void add_command_path(int num_path, char **token_path, char **token_line);
 
-/* TOOLS_4 */
+/* funcs_4 */
 void *_calloc(unsigned int nmemb, unsigned int size);
 void handle_sigint(int sig);
 void set_null(char *linea);
 
-/* TOOLS_5 */
+/* funcs_5 */
 char *no_spaces(char *linea);
 
 #endif
